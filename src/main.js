217b0a0b43c98d618b1dd479e94ef97a341587b7
dixit-star <dixit991752@gmail.com>
2025-12-31 08:46:12 +0530

    Replace product import with fetch from JSON
    Fetch product data from JSON file instead of importing.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import './style.css'
-import product from './product.json'
+// import product from './product.json'
 import { showProductConatiner } from './homeProductCard.js';
 //console.log(product);
 
-
+fetch("../src/product.json").then(res=>res.json())
+.then(data=>{
+    // console.log(data);
+    showProductConatiner(data)
+})
 
 // --------------show the temlets-------------
-showProductConatiner(product)
 
