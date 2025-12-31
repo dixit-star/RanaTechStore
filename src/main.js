7246a80f500e5d492a0c6bd98f3c795e0da45f3e
dixit-star <dixit991752@gmail.com>
2025-12-31 08:56:34 +0530

    Fix fetch path for product.json

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import { showProductConatiner } from './homeProductCard.js';
 //console.log(product);
 
-fetch("../src/product.json").then(res=>res.json())
+fetch("./product.json").then(res=>res.json())
 .then(data=>{
     // console.log(data);
     showProductConatiner(data)
